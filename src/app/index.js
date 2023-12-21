d93a78bc0e338523bdd74ce17891546dd009283c
xuguang-web <2529073813@qq.com>
2023-12-21 23:57:56 +0800

    fix: 添加跨域中间件

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,14 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
+const cors = require('koa2-cors')
 const registerRoutes = require('../router')
 const errorHandle = require('./error-handle')
 
 const app = new Koa()
 
 app.use(bodyParser())
+app.use(cors());
+
 registerRoutes(app)
 
 app.on('error', errorHandle)
